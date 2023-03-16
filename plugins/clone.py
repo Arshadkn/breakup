dac3ee36b1d9c246b1cddccc4f1f22f07c59ac16
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-16 20:15:06 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters, enums
 from info import ADMINS, API_ID, API_HASH
 import re
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from utlis import clone_temp
 
 @Client.on_message(filters.command("clone") & filters.user(ADMINS))
 async def delvarrrssz(bot, message):
@@ -17,14 +18,10 @@ async def delvarrrssz(bot, message):
         )
         await clone_bot.start()
         bot = await clone_bot.get_me()
-        details = {
-            'bot_id': bot.id,
-            'is_bot': True,
-            'user_id': user_id,
-            'name': bot.first_name,
-            'token': bot_token,
-            'username': bot.username
-        }
+        clone_temp.ME = bot.id
+        clone_temp.U_NAME = bot.username
+        clone_temp.B_NAME = bot.firstname
+            
         await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
     except BaseException as e:
         await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @Master_broi to be fixed.")
