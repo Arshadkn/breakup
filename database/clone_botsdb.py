f74adbcaf2f0255397c2f2d26d27a54e92b86975
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-23 08:23:05 +0530

    Create clone_botsdb.py

---
+++ database/clone_botsdb.py
@@ -0,0 +1,17 @@
+import pymongo
+import requests
+from info import DATABASE_URI, DATABASE_NAME
+
+
+
+
+client = pymongo.MongoClient(DATABASE_URI)
+db = client[DATABASE_NAME]
+bots_col = db["bots"]
+
+def save_bot_details(bot.username, bot_token):
+  
+
+    # Parse the response to extract the bot details
+    
+
