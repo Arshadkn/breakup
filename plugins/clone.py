fffa9e2b7c90d560b0d6ca314384c69fe4c9d714
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-23 08:30:26 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -18,9 +18,14 @@ async def delvarrrssz(bot, message):
         )
         await clone_bot.start()
         bot = await clone_bot.get_me()
-        clone_temp.ME = bot.id
-        clone_temp.U_NAME = bot.username
-        clone_temp.B_NAME = bot.firstname
+        details = {
+            'bot_id': bot.id,
+            'is_bot': True,
+            'user_id': user_id,
+            'name': bot.first_name,
+            'token': bot_token,
+            'username': bot.username
+        }
             
         await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
     except BaseException as e:
